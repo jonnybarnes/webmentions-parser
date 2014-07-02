b0bf1f9a3785b577560bbaeeb707a2755e9c8d98
Jonny Barnes <jonny@jonnybarnes.net>
2014-07-02 16:54:07 +0100

    Initial Tests

---
+++ tests/ParserTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use JonnyBarnes\WebmentionsParser\Parser;
+
+class ParserTest extends PHPUnit_Framework_TestCase {
+
+	public function testParser()
+	{
+		$html = 'text';
+		$parser = new Parser();
+		$this->assertEquals('text', $parser->parse($html);
+	}
+}
