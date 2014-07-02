d70ac9f00886dbbe4e4850d8ed49b7b5c876d11b
Jonny Barnes <jonny@jonnybarnes.net>
2014-07-02 17:22:20 +0100

    We should have a getMicroformats method

--- src/Parser.php
+++ src/Parser.php
@@ -2,11 +2,22 @@
 
 namespace JonnyBarnes\WebmentionsParser;
 
+use Mf2;
+
+class ParsingException {}
+
 class Parser {
 
-	public function parse($html)
+	public function getMicroformats($html)
 	{
-		return $html;
+		try {
+			$mf = \Mf2\parse($html);
+		} catch(Exception $e) {
+			//log $e maybe?
+			throw new ParsingException("php-mf2 failed to parse the HTML");
+		}
+
+		return $mf;
 	}
 
 }
