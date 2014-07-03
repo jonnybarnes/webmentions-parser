c784c5b5013088e9c443774284e0153c9a050f56
Jonny Barnes <jonny@jonnybarnes.net>
2014-07-03 01:10:31 +0100

    Implement a recursive array_key_exists to check for keys in

--- src/Parser.php
+++ src/Parser.php
@@ -4,10 +4,14 @@ namespace JonnyBarnes\WebmentionsParser;
 
 use Mf2;
 
-class ParsingException {}
+class ParsingException extends \Exception {}
 
 class Parser {
 
+	/**
+	 * What we really want to parse are the microformats, but here's a starter method
+	 * to deal with the original HTML.
+	 */
 	public function getMicroformats($html)
 	{
 		try {
@@ -18,6 +22,41 @@ class Parser {
 		}
 
 		return $mf;
+	}
+
+	/**
+	 * Return the type of mention or throw an error if undetermined
+	 */
+	public function getMentionType(array $mf)
+	{
+		if($this->array_key_exists_r('in-reply-to', $mf)) {
+			return 'in-reply-to';
+		}
+		if($this->array_key_exists_r('like-of', $mf)) {
+			return 'like-of';
+		}
+		if($this->array_key_exists_r('repost-of', $mf)) {
+			return 'repost-of';
+		}
+
+		//can't determine what type of mentio it is, throw exception
+		throw new ParsingException("Can't determine type of mention");
+	}
+
+	/**
+	 * Our recursive array_key_exists function
+	 */
+	private function array_key_exists_r($needle, $haystack)
+	{
+		$result = array_key_exists($needle, $haystack);
+		if($result) return $result;
+		foreach($haystack as $v) {
+			if(is_array($v)) {
+				$result = $this->array_key_exists_r($needle, $v);
+			}
+			if($result) return $result;
+		}
+		return $result;
 	}
 
 }
