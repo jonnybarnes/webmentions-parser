cdb25a4dfe5e29314d6828f7ab53eaf042238b45
Jonny Barnes <jonny@jonnybarnes.net>
2014-07-03 01:10:56 +0100

    A simple test to check mention type

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -4,22 +4,40 @@ use JonnyBarnes\WebmentionsParser\Parser;
 
 class ParserTest extends PHPUnit_Framework_TestCase {
 
-	public function testParser()
+	/**
+	 * Test determining mention types
+	 */
+	public function testMentionTypeReply()
 	{
-		$html = '<p class="h-card">Joe Bloggs</p>';
+		$html = '<!doctype html>
+<html>
+	<body class="h-entry">
+		<div class="p-in-reply-to h-cite">
+			<p>
+				<a class="u-url" rel="in-reply-to" href="http://billy.com/notes/2014/06/22/4/">
+				<time class="dt-published" datetime="2014-06-22T22:59:13-07:00">2014-06-22 22:59</time></a>
+			</p>
+		
+			<p class="p-author h-card">
+				<img class="u-photo" src="http://billy.com/images/billy.png" alt="" />		
+				<a class="u-url p-name" href="http://billy.com/">Billy</a>
+			</p>
+		
+			<div class="p-summary p-name e-content">
+				<img alt="" class="u-photo" src="http://billy.com/notes/2014/06/22/4/files/photo.jpg" />
+				Not bad...
+			</div>
+		</div>
+				
+		<div class="p-name entry-title p-summary summary e-content entry-content">
+			<p><a class="auto-link h-x-username" href="https://twitter.com/billy">@billy</a> Looks great</p>
+		</div>
+	</body>
+</html>';
 		$parser = new Parser();
-		$mf = $parser->getMicrofromats($html);
-		$expected = <<<'EOD'{
-	"items": [{
-		"type": ["h-card"],
-		"properties": {
-			"name": ["Joe Bloggs"]
-		}
-	}],
-	"rels": {}
-}
-EOD;
-		$this->assertEquals($expected, $parser->getMicroformats($html));
+		$mf = $parser->getMicroformats($html);
+		$expected = 'in-reply-to';
+		$this->assertEquals($expected, $parser->getMentionType($mf));
 	}
-	}
+
 }
