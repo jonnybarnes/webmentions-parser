da413405a7bd564b4cdffe59f47b3bd760e9126d
Jonny Barnes <jonny@jonnybarnes.net>
2014-08-14 16:00:00 +0100

    Trying one of sandeeps tests, Mocking Guzzle

--- tests/AuthorshipTest.php
+++ tests/AuthorshipTest.php
@@ -3,16 +3,32 @@
 use Jonnybarnes\WebmentionsParser\Authorship;
 use Jonnybarnes\WebmentionsParser\Parser;
 
+use GuzzleHttp\Adapter\MockAdapter;
+use GuzzleHttp\Adapter\TransactionInterface;
+use GuzzleHttp\Message\Response;
+
 class AuthorshipTest extends PHPUnit_Framework_TestCase {
 
 	private $dir = __DIR__;
 
 	public function testAlgo()
 	{
-		$html = file_get_contents($this->dir . '/HTML/testAuthorshipAlgo.html');
+		$mock = new MockAdapter(function() {
+			$html = file_get_content($this->dir . '/HTML/authorship-test-cases/h-card_with_u-url_that_is_also_rel-me');
+
+			return new Response(200, null, $html);
+		})
+		$html = file_get_contents($this->dir . '/HTML/authorship-test-cases/h-entry_with_rel-author_pointing_to_h-card_with_u-url_that_is_also_rel-me.html');
 		$parser = new Parser();
 		$auth = new Authorship();
 		$mf = $parser->getMicroformats($html);
-		$this->assertEquals(true, $auth->findAuthor($mf));
+		try {
+			$auth->findAuthor($html);
+		} catch(Exception $e) {
+			var_dump($e);
+			return fail;
+		}
+
+		return pass;
 	}
 }
