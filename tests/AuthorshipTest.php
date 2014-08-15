ced91962cc6946f1b1fba8085ce9ec1109a5f200
Jonny Barnes <jonny@jonnybarnes.net>
2014-08-15 10:57:42 +0100

    Added third test

--- tests/AuthorshipTest.php
+++ tests/AuthorshipTest.php
@@ -72,4 +72,41 @@ class AuthorshipTest extends PHPUnit_Framework_TestCase {
 
 		$author = $auth->findAuthor($mf);
 	}
+
+	public function testHEntryWithRelAuthorPointingToHCardWithUUrlEqualToUUidEqualToSelf()
+	{
+		$mock = new MockAdapter(function() {
+			$mockhtml = file_get_contents($this->dir . '/HTML/authorship-test-cases/h-card_with_u-url_equal_to_u-uid_equal_to_self.html');
+			$stream = Stream\create($mockhtml);
+
+			return new Response(200, array(), $stream);
+		});
+		$html = file_get_contents($this->dir . '/HTML/authorship-test-cases/h-entry_with_rel-author_pointing_to_h-card_with_u-url_equal_to_u-uid_equal_to_self.html');
+		$parser = new Parser();
+		$auth = new Authorship();
+		$auth->mockAdapter($mock);
+		$mf = $parser->getMicroformats($html);
+
+		$expected = array(
+			'type' => array(
+				'h-card'
+			),
+			'properties' => array(
+				'name' => array(
+					'John Doe'
+				),
+				'url' => array(
+					'h-card_with_u-url_equal_to_u-uid_equal_to_self.html'
+				),
+				'uid' => array(
+					'h-card_with_u-url_equal_to_u-uid_equal_to_self.html'
+				),
+				'photo' => array(
+					'http://www.gravatar.com/avatar/fd876f8cd6a58277fc664d47ea10ad19.jpg?s=80&d=mm'
+				)
+			)
+		);
+
+		$this->assertEquals($expected, $author = $auth->findAuthor($mf));
+	}
 }
