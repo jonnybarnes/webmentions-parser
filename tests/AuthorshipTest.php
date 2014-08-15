213292c6238f3a93317dda947930fc66748c6c47
Jonny Barnes <jonny@jonnybarnes.net>
2014-08-15 10:32:32 +0100

    First proper test added

--- tests/AuthorshipTest.php
+++ tests/AuthorshipTest.php
@@ -12,25 +12,41 @@ class AuthorshipTest extends PHPUnit_Framework_TestCase {
 
 	private $dir = __DIR__;
 
-	public function testAlgo()
-	{
-		$mock = new MockAdapter(function() {
+		/*$mock = new MockAdapter(function() {
 			$mockhtml = file_get_contents($this->dir . '/HTML/authorship-test-cases/h-card_with_u-url_that_is_also_rel-me.html');
 			$stream = Stream\create($mockhtml);
 
 			return new Response(200, array(), $stream);
-		});
-		$html = file_get_contents($this->dir . '/HTML/authorship-test-cases/h-entry_with_rel-author_pointing_to_h-card_with_u-url_that_is_also_rel-me.html');
+		});*/
+
+	public function testHEntryWithPAuthor()
+	{
+		$html = file_get_contents($this->dir . '/HTML/authorship-test-cases/h-entry_with_p-author.html');
 		$parser = new Parser();
 		$auth = new Authorship();
-		$auth->mockAdapter($mock);
 		$mf = $parser->getMicroformats($html);
-		try {
-			$author = $auth->findAuthor($mf);
-		} catch (AuthorException $e) {
-			$author = false;
-		}
 
-		$this->assertFalse($author);
+		$expected = array(
+			array(
+				'type' => array(
+					'h-card'
+				),
+				'properties' => array(
+					'name' => array(
+						'John Doe'
+					),
+					'url' => array(
+						'http://example.com/johndoe/'
+					),
+					'photo' => array(
+						'http://www.gravatar.com/avatar/fd876f8cd6a58277fc664d47ea10ad19.jpg?s=80&d=mm'
+					)
+				),
+				'value' => 'http://www.gravatar.com/avatar/fd876f8cd6a58277fc664d47ea10ad19.jpg?s=80&d=mm
+			John Doe'
+			)
+		);
+
+		$this->assertEquals($expected, $auth->findAuthor($mf));
 	}
 }
